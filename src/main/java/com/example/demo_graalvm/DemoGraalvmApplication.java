7d5e5b084063c99cfc8656cb39c91b1554099739
up1 <somkiat.p@gmail.com>
2023-02-16 23:13:58 +0700

    Let's start

---
+++ src/main/java/com/example/demo_graalvm/DemoGraalvmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGraalvmApplication.class, args);
+	}
+
+}
